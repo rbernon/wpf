4dd50c24a4b18222a7467a3c81ca825e099d8f89
Rémi Bernon <rbernon@codeweavers.com>
2019-06-12 13:20:17 +0200

    mono: Build with custom Makefile for Mono

---
+++ src/Microsoft.DotNet.Wpf/src/System.SR.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+namespace System
+{
+    internal static partial class SR
+    {
+        private static global::System.Resources.ResourceManager s_resourceManager;
+        internal static global::System.Resources.ResourceManager ResourceManager => s_resourceManager ?? (s_resourceManager = new global::System.Resources.ResourceManager("SR", Assembly.GetExecutingAssembly()));
+        internal static global::System.Globalization.CultureInfo Culture { get; set; }
+
+        [global::System.Runtime.CompilerServices.MethodImpl(global::System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+        internal static string GetResourceString(string resourceKey, string defaultValue = null) =>  ResourceManager.GetString(resourceKey, Culture);
+		}
+}
